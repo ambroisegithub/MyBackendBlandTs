5090a183e913ac1707bd7ae561ea6f5b69f14832
ambroisegithub <muhayimana21@gmail.com>
2024-03-14 14:07:16 +0200

    jest and supertest testing

--- index.ts
+++ index.ts
@@ -1,7 +1,16 @@
-import app from "./app";
+
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import express, { Application, Request, Response } from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import  { Request, Response } from "express";
+import commentRoutes from "./src/Routes/commentRoutes";
+import userRoutes from "./src/Routes/UserRoute";
+import blogRoutes from "./src/Routes/BlogRoute";
+import contactUsRoutes from "./src/Routes/contactUsRoutes";
+import subscribeRoutes from "./src/Routes/subscribeRoute";
+import cors from "cors";
+import helmet from "helmet";
 
 dotenv.config();
 
@@ -11,13 +20,30 @@ mongoose
   .then(() => {
     console.log("DB CONNECTED");
   })
-  .catch((err:Error) => {
+  .catch((err: Error) => {
     console.log(err);
   });
-  const PORT:number = Number(process.env.PORT);
-  app.listen(PORT,()=>{
-    console.log(`The Server is connected on ${PORT}`)
-  })
+
+const app: Application = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use('/api/user', userRoutes);
+app.use('/api/blog', blogRoutes);
+app.use('/api/blog', commentRoutes);
+app.use('/api/contactus', contactUsRoutes);
+app.use('/api/subscribe', subscribeRoutes);
+
+app.get("/", (req: Request, res: Response) => {
+  return res.json({ message: "Welcome To My portfolio API" });
+});
+
+const PORT: number = Number(process.env.PORT);
+app.listen(PORT, () => {
+  console.log(`The Server is connected on ${PORT}`);
+});
 
 
-  export default app;
