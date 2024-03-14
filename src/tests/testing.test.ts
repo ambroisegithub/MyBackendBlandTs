5b23eaf7f0c797283372d0fe2ea95f96bd5350af
ambroisegithub <muhayimana21@gmail.com>
2024-03-14 14:48:51 +0200

    jest and supertest testing

---
+++ src/tests/testing.test.ts
@@ -0,0 +1,21 @@
+
+import supertest from "supertest";
+import { app, server, connectToDatabase } from "./index.test";
+
+const request = supertest(app);
+
+describe("GET /", () => {
+  beforeAll(async () => {
+    await connectToDatabase(); 
+  });
+
+  afterAll((done) => {
+    server.close(done);
+  });
+
+  it("responds with JSON message", async () => {
+    const response = await request.get("/");
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ message: "Welcome To My portfolio API" });
+  });
+});
